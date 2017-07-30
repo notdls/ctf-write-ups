b0dfafc68456dd28d3cbe131a58f4481571bc01c
notdls <dlsvuln@gmail.com>
2017-07-30 20:19:50 +1000

    Create flag.php

---
+++ Bugs_BunnyCTF-2017/web/SimplePHP-80/flag.php
@@ -0,0 +1,4 @@
+<?php
+// This isn't the real flag.php, just a guess at what it could've contained (that being said the flag below isn't the real flag)
+$flag = "Bugs_Bunny{This_isnt_the_real_flag}";
+?>
